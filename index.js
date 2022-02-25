ff6ef7ad13c4a6d2a9ae6868d63cc1cadad25f7b
Rachakonda Sai <72305047+VaishnaviRachakonda@users.noreply.github.com>
2022-02-25 09:12:59 +0000

    Accepted solution

--- index.js
+++ index.js
@@ -6,18 +6,29 @@
 var compareVersion = function(version1, version2) {
     let v1 = version1.split('.');
     let v2 = version2.split('.');
-    let len= v1.length>=v2.length?v1.length:v2.length;
-    for(let i=0;i<len;i++){
-     if(parseInt(v1[i])!==parseInt(v2[i])){
-         if(parseInt(v1[i])===NaN && parseInt(v2[i])) return -1
-        else if (parseInt(v2[i])===NaN && parseInt(v1[i])) return 1
-           else if(parseInt(v1[i])>parseInt(v2[i])) {
-                return 1
-            }
-            else if(parseInt(v1[i])<parseInt(v2[i])){
-                return -1
-            }
-        }
+    if(v1.length>v2.length){
+      while(v1.length!==v2.length){
+        v2.push('0')
+      }
     }
-        return 0
+  else if(v2.length>v1.length){
+    while(v1.length!==v2.length){
+        v1.push('0')
+      }
+  }
+  for(let i=0;i<v1.length;i++){
+    if(parseInt(v1[i])>parseInt(v2[i])){
+      return 1
+    }
+    else if(parseInt(v1[i])< parseInt(v2[i])){
+      return -1
+    }
+  }
+ return 0
 };
+
+console.log('1',compareVersion('1.1.1','1.1'))//1
+console.log('2',compareVersion('1.1','1'))//1
+console.log('3',compareVersion('1.0.0','1'))//0
+console.log('4',compareVersion('1.0.0','1.1'))//-1
+console.log('5',compareVersion('1.0.0','1.0.0.0.1'))//-1
